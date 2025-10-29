8da8e5dc3a1ce62b79f817b0d41e3cf2fa7c68bb
Marcus3588 <marcee3588@gmail.com>
2025-10-29 21:59:25 +0000

    Updated Playlist

---
+++ music-player/scripts/player.js
@@ -0,0 +1,43 @@
+export default class player {
+	constructor(playlist) {
+		this.playlist = playlist;
+		this.index = 0;
+		this.audio = new Audio(playlist[this.index].src);
+		this.shuffleMode = false;
+		this.repeatMode = false
+	}
+	play(){
+		this.audio.play();
+	}
+	pause(){
+		this.audio.pause();
+	}
+	next(){
+		if(this.shuffleMode) {
+			this.index = Math.floor(Math.random()*this.playlist.length);
+		} else{
+			this.index = (this.index+1) %this.playlist.length;
+		}
+		this.loadCurrentSong();
+		this.play();
+	}
+	prev(){
+		this.index = (this.index-1 + this.playlist.length)  % this.playlist.length;
+		this.loadCurrentSong();
+		this.play();
+	}
+	toggleShuffle(){
+		this.shuffleMode = !this.shuffleMode;
+		return this.shuffleMode;
+	}
+	toggleRepeat(){
+		this.repeatMode = !this.repeatMode;
+		return this.repeatMode;
+	}
+	loadCurrentSong(){
+		this.audio.src = this.playlist[this.index].src;
+	}
+	setVolume(value) {
+		this.audio.volume = value;
+	}
+}
---
+++ music-player/scripts/playlistManager.js
@@ -0,0 +1,21 @@
+export const playlist = [
+	{
+		title:'Lost Stars',
+		artist: 'Adam Levin',
+		src: 'https://www2.cs.uic.edu/~i101/SoundFiles/StarWars3.wav',
+		cover: 'https://images.unsplash.com/photo-152170588-4d8ecf77b99f?w=600'
+	},
+	{
+		title:'Falling Slowly',
+		artist: 'Glen Hansard',
+		src: 'https://www2.cs.uic.edu/~i101/SoundFiles/CantinaBand3.wav',
+		cover: 'https://images.unsplash.com/photo-1507874457470-272b3c8dee2?w=600'
+	},
+	{
+	title:'Skyline Dreams',
+		artist: 'Instrumental',
+		src: 'https://www2.cs.uic.edu/~i101/SoundFiles/BabyElephantWalk60.wav',
+		cover: 'https://images.unsplash.com/photo-1511671782779-c97d3d27a1d4?w=600'
+	}
+]
+
